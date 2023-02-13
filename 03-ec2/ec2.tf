a28e72150d4c3ace44b3e0d70c4d19435672d3c4
Harshitha <harshithadarshan95@gmail.com>
2023-02-13 11:52:40 +0530

    Autopush

---
+++ 03-ec2/ec2.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# creates ec2 instance
+resource "aws_instance" "my-ec2" {
+  ami                     = "ami-0e3aeafe193bbdd4a"
+  instance_type           = "t2.micro"
+  vpc_security_group_ids  = [aws_security_group.allow_ssh.id]
+
+  tags = {
+      Name = "Terraform-Server"
+  }
+}
+
+
+# Creates Security Group
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description = "SSH from Public"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]   # [] represent's list. 
+  }
+
+  ingress {
+    description = "HTTP from Public"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]   # [] represent's list. 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_sg"
+  }
+}
+
+
+# Prining the public ip address of the machine
+output "public_dns_output" {
+  value   =  aws_instance.my-ec2.public_dns
+}
