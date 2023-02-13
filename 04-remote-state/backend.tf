2890781f0e960ae5ba2a0b361aaf74733b77fe7e
Harshitha <harshithadarshan95@gmail.com>
2023-02-13 12:04:43 +0530

    Autopush

--- 04-remote-state/backend.tf
+++ 04-remote-state/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "b52-terraform-state-bucket"
+    bucket = "b52-terraform-state-bucket1"
     key    = "ec2/dev/terraform.tfstate"
     region = "us-east-1"
     }
